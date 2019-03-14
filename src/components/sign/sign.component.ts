e4527eb2fbed60e912691319532f6634883450e1
王乃强 <sadwadf@qq.com>
2019-03-14 18:27:10 +0800

    发文-发文签

--- src/components/sign/sign.component.ts
+++ src/components/sign/sign.component.ts
@@ -7,11 +7,20 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SignComponent implements OnInit {
   @Input() itemmodel: any;
+  @Input() documenttype: any;
+  title: string;
   constructor() {
     console.log('构造函数');
   }
 
   ngOnInit() {
-    console.log('进入init方法');
+    console.log(this.itemmodel);
+    if (this.itemmodel.documenttype === 1) {
+      this.title = '收文登记表';
+    } else if (this.itemmodel.documenttype === 2) {
+      this.title = '发文拟稿';
+    } else if (this.itemmodel.documenttype === 3) {
+      this.title = '发文拟稿';
+    }
   }
 }
