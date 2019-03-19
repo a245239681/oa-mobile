d41d1be9463fc1243b47b84beb47d7ed085b1f42
黄其程 <Master@DESKTOP-NAODEB8>
2019-03-19 22:49:47 +0800

    搜索通信录、发送短信，呼叫号码

--- src/pages/mail-list/mail-list.page.ts
+++ src/pages/mail-list/mail-list.page.ts
@@ -25,10 +25,22 @@ export class MailListPage implements OnInit {
    */
   num: any;
 
+  allListModel: any;
+
+    /**
+   * 搜索
+   */
+  searchStr = '';
+
   /**
    * 标题
    */
   title = '部门相关人员';
+
+   /**
+   * 搜索结果
+   */
+  result = [];
 
   constructor(
     private activeRoute: ActivatedRoute,
@@ -37,6 +49,7 @@ export class MailListPage implements OnInit {
     this.activeRoute.queryParams.subscribe((params: Params) => {
       this.itemmodel = JSON.parse(params['item']);
       this.deptModel = JSON.parse(params['dept']);
+      this.allListModel = JSON.parse(params['allList']);
       this.num = this.itemmodel.length;
       console.log(this.itemmodel);
     });
@@ -52,6 +65,12 @@ export class MailListPage implements OnInit {
         console.log(e.mobile);
         this.callIpnone(e.mobile);
         // console.log('Delete clicked');
+      }
+    },
+    {
+      text: '发送短信至' + e.mobile,
+      handler: () => {
+        document.location.href = 'mms:' + e.mobile;
       }
     },
     {
@@ -77,6 +96,29 @@ export class MailListPage implements OnInit {
     });
 
     await actionSheet.present();
+  }
+
+    /**
+   * 搜索
+   */
+  SearchFilter(event: any) {
+    const val = event.toLowerCase();
+    // filter our data
+    this.result = [];
+    const me = this;
+    this.allListModel.forEach(function(d: any) {
+      const items = d.children.filter(s => s.text.indexOf(val) !== -1
+       || s.mobile.indexOf(val) !== -1
+       || ('' + s.phone).indexOf(val) !== -1 );
+      if ( items.length > 0) {
+        me.result.push({
+          text: d.text,
+          children: items
+        });
+      }
+    });
+    // console.log(this.result);
+
   }
 
 
